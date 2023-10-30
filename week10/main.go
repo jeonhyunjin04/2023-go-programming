96a6ab15a31d166cf4adb2c1c38ee7769d583ad7
wjsguswls <wjsguswls04@naver.com>
2023-10-30 11:38:33 +0900

    week10 v1.9

--- week10/main.go
+++ week10/main.go
@@ -1,8 +1,11 @@
 package main
 
 import "week10/src/greeting"
+	   "week10/src/mymath"
 
 func main() {
 	greeting.Hello()
+	fmt.Println(mymath.MyAbs(99))
+	fmt.Println(mymath.MyAbs(-7))
 	greeting.Hi()
 }
---
+++ week10/src/mymath/mymath.go
@@ -0,0 +1,9 @@
+package mymath
+
+import "fmt"
+
+func Myabs(number int) int {
+	if number < 0 {
+		return number *-1
+	}
+}
