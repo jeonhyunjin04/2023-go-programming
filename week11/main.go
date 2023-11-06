21a80db6c517fd35254feed3a29ea72dd7e8515c
wjsguswls <wjsguswls04@naver.com>
2023-11-06 10:51:54 +0900

    week11 v2.5

--- week11/main.go
+++ week11/main.go
@@ -6,9 +6,13 @@ func main() {
 	primes := [3]int{2, 3, 5} //initialize by array literal
 	//primes[2] = 6
 	for i := 0; i < 3; i++ {
-		fmt.Println(primes[i])
+		fmt.Println(i, primes[i])
 	}
 	// 초기화 하지 않은 원소의 제로 값은 해당 원소 타입의 제로값으로 결정된다.
 	test := [5]bool{true, true, true}
-	fmt.Println(test[1])
+	fmt.Println(test[3])
+	fmt.Println(test)
+
+	fmt.Printf("%#v\n", primes)
+	fmt.Printf("%#v", test)
 }
