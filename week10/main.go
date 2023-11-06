3c6bf7ca4e15a946b7055298be36c3f3ab149a5b
wjsguswls <wjsguswls04@naver.com>
2023-11-06 10:40:19 +0900

    week10 v2.2

--- week10/main.go
+++ week10/main.go
@@ -1,9 +1,13 @@
 package main
 
-import "week10/src/greeting"
-	   "week10/src/mymath"
+import (
+	"fmt"
+	"week10/src/greeting"
+	"week10/src/mymath"
+)
 
 func main() {
+	fmt.Println(mymath.MyPower(2, 10))
 	greeting.Hello()
 	fmt.Println(mymath.MyAbs(99))
 	fmt.Println(mymath.MyAbs(-7))
--- week10/src/greeting/greeting.go
+++ week10/src/greeting/greeting.go
@@ -2,7 +2,8 @@ package greeting
 
 import "fmt"
 
-func Hello() {
+//func hello() {
+func Hello() { // 함수의 이름 첫 글자를 대문자로 해야 외부 파일에서 참조 할 수 있다
 	fmt.Println("안녕하세요!")
 }
 
