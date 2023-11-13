05a38fe49e2a3a31f4babf4dc269818c4d74874b
wjsguswls <wjsguswls04@naver.com>
2023-11-13 11:38:16 +0900

    week12 v3.5

--- week12/main.go
+++ week12/main.go
@@ -3,24 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	s := []int{0, 0, 0, 0, 0} // slice literal
+	a := []string{"a", "b", "c", "d"}
+	as := a[0:2] // a, b
+	as[1] = "z"
+	fmt.Println(as)
 
-	s[4] = 99
-	s[2] = 11
-
-	for i := 0; i < len(s); i++ {
-		fmt.Println(s[i])
-	}
-
-	copyS := s[1:4]
-	for _, value := range copyS {
-		fmt.Println(value)
-	}
-
-	test := [3]string{"inha", "go", "student"}
-	//testS := test[0:4] // error invalid argument : index 4 out of bounds [0:4]
-	testS := test[:2] //testS := test[0:2]
-	testS2 := test[1:]
-	fmt.Println(testS2[1])
-	fmt.Println(len(testS))
+	b := [4]int{4, 3, 2, 1}
+	bs := b[1:3]
+	fmt.Println(bs)
 }
