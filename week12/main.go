4bb626649893f53ede8d1a9420965912d24e0b21
wjsguswls <wjsguswls04@naver.com>
2023-11-13 10:55:53 +0900

    week12 v3.3

--- week12/main.go
+++ week12/main.go
@@ -3,9 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	s := []int{0, 0, 0, 0, 0}
+	s := []int{0, 0, 0, 0, 0} // slice literal
 
-	for _, value := range s {
+	s[4] = 99
+	s[2] = 11
+
+	for i := 0; i < len(s); i++ {
+		fmt.Println(s[i])
+	}
+
+	copyS := s[1:4]
+	for _, value := range copyS {
 		fmt.Println(value)
 	}
 
